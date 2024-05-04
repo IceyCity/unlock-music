508489f262c061971800f903bc1bd3965215fb6c
IceyCity <1201741226@cug.edu.cn>
2024-05-05 01:02:32 +0800

    初始化仓库

---
+++ precache-manifest.69603753abac0cbcc451179235afea2c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693d9635cded3fa31873",
+    "url": "css/app.f67c2d44.css"
+  },
+  {
+    "revision": "33d12a78ab71406c1b57",
+    "url": "css/chunk-vendors.094863c6.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "a32c9dabec4a6e6a50d8df42831041ec",
+    "url": "index.html"
+  },
+  {
+    "revision": "cfce103133e001de644ebb74900ec950",
+    "url": "js/0.bf78a994.worker.js"
+  },
+  {
+    "revision": "693d9635cded3fa31873",
+    "url": "js/app.ea83fbb0.js"
+  },
+  {
+    "revision": "33d12a78ab71406c1b57",
+    "url": "js/chunk-vendors.5db7ca01.js"
+  },
+  {
+    "revision": "02995355b96ddf2519cd49f8aa73bb46",
+    "url": "loader.js"
+  },
+  {
+    "revision": "cd1d395410107c66b4534ec93f0073d3",
+    "url": "web-manifest.json"
+  }
+]);
